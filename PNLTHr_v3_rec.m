d44cebc0a22806cb3abc2c9d58af5707bcff5ea4
Timur <timur.bazarov@phystech.edu>
2020-05-20 21:06:01 +0300

    Calculation of one disp curve. Bugs must be fixed.

--- PNLTHr_v3_rec.m
+++ PNLTHr_v3_rec.m
@@ -6,9 +6,21 @@ tStart=tic;
 
 load('optLibs.mat');
 %Open start file, where the scheme will be drawn
-directory = strcat(pwd,'\PNLThr_start_for_save.osd');
+directory = strcat(pwd,'\PNLThr_start_for_load.osd');
 optsys=OpenOptisystem(directory);
 Npars=1;%always 1, because parallel calculations weren't implemented
+
+
+fileName='05-20-2020_20_25_53_N_of_Chs=3_Pin=1-10.ods';
+a2=regexp(fileName,'_Pin')-1;
+a1=regexp(fileName,'Chs=')+4;
+b2=regexp(fileName,'.ods')-1;
+b1=regexp(fileName,'_Pin=')+5;
+N=str2num(fileName(a1:a2));
+temp=split(fileName(b1:b2),"-");
+Pmin=str2num(temp{1});
+Pmax=str2num(temp{2});
+NPin=Pmax-Pmin+1;
 
 %Get main system variables
 Document = optsys.GetActiveDocument;
@@ -45,14 +57,6 @@ NDisps=length(Disps);
 NDisp=length(Disps);%number of dispersion points
 BERreq=10^-10;%value of req BER
 dDispThr=200;%threshold for dispersion curve variations
-
-Pin=[0,1];%array with param of channels power after input amplifier
-    if (mod(length(Pin),Npars)==0) 
-        Pin=reshape(Pin,Npars,length(Pin)/Npars);
-    else
-        error='size of Pin is not agreed with Npars';
-        return;
-    end
  
 %drawing settings
 compW=34;%width of a Component
@@ -61,86 +65,81 @@ xL0=10;%start coodinate of first column of dynamic Components
 yL0=60;%start coodinate of first row of dynamic Components
 dx=100;%horizontal distance between dynamic Components
 dy=200;%vertical distance between dynamic Components
-xEye=1000;%x coordinate of BER analyzer
-yEye=100;%y coordinate of BER analyzer
 
-%customizing lasers of transmitter
-LasArr = Canvas.GetComponentByName('CW Laser Array');
-LasArr.SetParameterValue('Number of output ports',N);
-Layout.AddParameter('OutChP', 2, 'Global', -20, 10,'0', 'dBm','');
-Layout.SetParameterValue('OutChP',OutputOfOneLaser);
-for k=1:N
-    TrFreqName=['Frequency[',num2str(k-1),']'];
-    TrPowerName=['Power[',num2str(k-1),']'];
-    LasArr.SetParameterValue(TrFreqName,ch2Hz(Chs(k)));
-    LasArr.SetParameterMode(TrPowerName,3);%set scripted mode
-    LasArr.SetParameterScript(TrPowerName,'OutChP');%parameter will get value from variable OutChP
-end
+OSNRController=Canvas.GetComponentByName('Set OSNR');
+OSNRController.SetParameterValue('Signal Frequency',ch2Hz(AnCh));
 
+NFork=Canvas.GetComponentByName('Fork 1xN');
+NFork.SetParameterValue('Number of output ports',NDisps);
 
-%custmizing Mux, PRBSs, NRZgeneratos and modulators. Connecting them
-PRBSName='Pseudo-Random Bit Sequence Generator';
-NRZGenName='NRZ Pulse Generator';
-ModName='EA Modulator Analytical';
-Layout.AddParameter('InChirp', 2, 'Global', -2, 2,'0', '','');
-Layout.SetParameterValue('InChirp',Chirp0);
-Layout.AddParameter('Inm', 2, 'Global', -2, 2,'0', '','');
-Layout.SetParameterValue('Inm',m0);
+bgName='Ideal Dispersion Compensation FBG';
+optFiltName='Gaussian Optical Filter';
+optAmpName='Optical Amplifier';
+APDName='APD';
+LPFName='Low Pass Bessel Filter';
+RegName='3R Regenerator';
+EyeName='BER Analyzer';
+numCol=10;
 
-MuxName='Ideal Mux';
-Mux=Canvas.CreateComponent(MuxName,MuxLib,xL0+dx*4,yL0+dy*(2-0.5), compW, compH,1);
-Mux.SetParameterValue('Number of input ports',N);
-for k=1:N
-    %creating components
-    PRBSs(k) = Canvas.CreateComponent(PRBSName,TransLib,xL0+dx,yL0+dy*(k-1), compW, compH,1);
-    NRZGens(k) = Canvas.CreateComponent(NRZGenName,TransLib,xL0+dx*2,yL0+dy*(k-1), compW, compH,1);
-    Mods(k) = Canvas.CreateComponent(ModName,TransLib,xL0+dx*2,yL0+dy*(k-0.5), compW, compH,1);
+%draw receivers
+for k=1:NDisps
+    BG(k)=Canvas.CreateComponent(bgName,DispCompLib,xL0+numCol*dx,yL0+dy*(k-1), compW, compH,1);
+    OptFilt(k)=Canvas.CreateComponent(optFiltName,FiltLib,xL0+(numCol+1)*dx,yL0+dy*(k-1), compW, compH,1);
+    OptAmp(k)=Canvas.CreateComponent(optAmpName,AmpLib,xL0+(numCol+2)*dx,yL0+dy*(k-1), compW, compH,1);
+    APD(k)=Canvas.CreateComponent(APDName,RecLib,xL0+(numCol+3)*dx,yL0+dy*(k-1), compW, compH,1);
+    LPF(k)=Canvas.CreateComponent(LPFName,FiltLib,xL0+(numCol+4)*dx,yL0+dy*(k-1), compW, compH,1);
+    Reg(k)=Canvas.CreateComponent(RegName,RecLib,xL0+(numCol+5)*dx,yL0+dy*(k-1), compW, compH,1);
+    EyeOsc(k)=Canvas.CreateComponent(EyeName,VisLib,xL0+(numCol+7)*dx,yL0+dy*(k-1), compW, compH,1);
     
-    %customizing modulator
-    Mods(k).SetParameterMode('Modulation index',3);
-    Mods(k).SetParameterMode('Chirp factor',3);
-    Mods(k).SetParameterScript('Modulation index','Inm');
-    Mods(k).SetParameterScript('Chirp factor','InChirp');
+    %customizing recievers
+    BG(k).SetParameterValue('Frequency',ch2Hz(AnCh));
+    BG(k).SetParameterValue('Bandwidth',100*(N+2));
+    BG(k).SetParameterValue('Dispersion',Disps(k));
+    OptFilt(k).SetParameterValue('Frequency',ch2Hz(AnCh));
+    OptFilt(k).SetParameterValue('Bandwidth',FiltBand);
+    APD(k).SetParameterValue('Gain',APDGain);
+    APD(k).SetParameterValue('Responsivity',APDResp);
+    APD(k).SetParameterValue('Ionization ratio',APDIon);
+    OptAmp(k).SetParameterValue('Operation mode', 'Power control');
+    OptAmp(k).SetParameterValue('Power', OuputPower);
+    OptAmp(k).SetParameterValue('Noise figure',NF);
+    %LPF(k).SetParameterValue('Cutoff frequency','0.75*Symbol rate');
     
     %connecting components
-    LasArr.GetOutputPort(k).Connect(Mods(k).GetInputPort(2));
-    PRBSs(k).GetOutputPort(1).Connect(NRZGens(k).GetInputPort(1));
-    NRZGens(k).GetOutputPort(1).Connect(Mods(k).GetInputPort(1));
-    Mux.GetInputPort(k).Connect(Mods(k).GetOutputPort(1));
+    NFork.GetOutputPort(k).Connect(BG(k).GetInputPort(1));
+    BG(k).GetOutputPort(2).Connect(OptFilt(k).GetInputPort(1));
+    OptFilt(k).GetOutputPort(1).Connect(OptAmp(k).GetInputPort(1));
+    OptAmp(k).GetOutputPort(1).Connect(APD(k).GetInputPort(1));
+    APD(k).GetOutputPort(1).Connect(LPF(k).GetInputPort(1));
+    LPF(k).GetOutputPort(1).Connect(Reg(k).GetInputPort(1));
+    Reg(k).GetOutputPort(1).ConnectVisualizer(EyeOsc(k).GetInputPort(1));
+    Reg(k).GetOutputPort(2).ConnectVisualizer(EyeOsc(k).GetInputPort(2));
+    Reg(k).GetOutputPort(3).ConnectVisualizer(EyeOsc(k).GetInputPort(3));
 end
 
-%connecting mux with input amplifier
-InAmp=Canvas.GetComponentByName('Optical Amplifier');
-InAmp.GetInputPort(1).Connect(Mux.GetOutputPort(1));
-%InAmp.SetParameterValue('Power', 0+10*log10(N));%set total input power
-
-optFiber=Canvas.GetComponentByName('Optical Fiber CWDM');
-optFiber.SetParameterValue('Max. nonlinear phase shift',maxNLph);
-
-
-NPin=length(Pin);
-Layout.SetTotalSweepIterations(NPin);
-for k=1:lNPin
-    InAmp.SetSweepParameterValue('Power',k,Pin(k));
-end
-
-Document.Save(strcat(pwd,'\PNLThr_ForCalc_Fiber.osd'));
 Canvas.UpdateAll;%draw all created components and connections
+Document.Save(strcat(pwd,'\PNLThr_ForCalc.osd'));
 optsys.ActivateApplication();
 
+LoadFromFile=Canvas.GetComponentByName('Load from file');
+LoadFromFile.SetParameterValue('Filename',fileName);
 
-SaveFileComp=Canvas.GetComponentByName('Save to file');
-fileName=[datestr(now,'mm-dd-yyyy_HH-MM-SS'),sprintf('_N_of_Chs=%d_Pin=%d:%d.ods',N,min(Pin),max(Pin))];
-SaveFileComp.SetParameterValue('Filename',fileName);
+OSNRs=15:1:50;
+NOSNRs=length(OSNRs);
+data_BER=zeros(NPin,NDisps,length(OSNRs));
+q=0;
+
+Document.Save(strcat(pwd,'\PNLThr_ForCalc_Rec.osd'));
+LoadFromFile.SetParameterValue('Number of signals to skip',q);
 for m=1:NOSNRs
     OSNRController.SetParameterValue('Set OSNR',OSNRs(m));
     Document.CalculateProject( true , true);
     for k=1:NDisps
-        data_BER(k,m)=EyeOsc(k).GetResultValue('Min. BER');
+        data_BER(q+1,k,m)=EyeOsc(k).GetResultValue('Min. BER');
     end
 end
 
-Document.Save(strcat(pwd,'\Res\PNLThr_ForCalc.osd'));
+Document.Save(strcat(pwd,'\PNLThr_ForCalc_Rec.osd'));
 optsys.Quit;
 clear optsys;
 timeOfCals=toc(tStart);% time of calculations
